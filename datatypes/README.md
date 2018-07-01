137ef0702c7c947381ea652ff7a28b3115de2391
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2018-07-01 10:57:13 +0300

    Add Python skeleton and solutions

--- datatypes/README.md
+++ datatypes/README.md
@@ -4,7 +4,9 @@ Write a program that sends the highlighted elements of a 2D array
 using user defined datatypes from one MPI task to another. Note the
 different assignments for C and Fortran, and remember that C stores
 arrays in a row-major order and Fortran in a column-major order. You can
-start from skeleton codes in [C](./c) or [Fortran](./fortran)
+start from skeleton codes in [C](./c), [Fortran](./fortran) or
+[Python](./python). For Python use the C-patterns in a) and b) (`Note`: no
+Python solutions are provided for c) and d) )
 
 a) 
 
---
+++ datatypes/python/skeleton.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+from mpi4py import MPI
+import numpy as np
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+if size != 2:
+    raise RuntimeError("Please run with two MPI tasks")
+
+data = np.zeros((8,8), int)
+if rank == 0:
+    for i in range(8):
+        data[i,:] = np.arange(1, 9) + (i+1) * 10
+
+if rank == 0:
+    print("Original data:")
+    print(data)
+
+# TODO Create the custom datatype
+# Note: Python integers are 64 bits
+
+# TODO: communicate with the datatype
+# Note: mpi4py requires that the input and output buffers are contiguous 
+# in memory. Thus, in order to send/receive from second column we need
+# to create a contiguous view starting from the second element in memory
+# which can be accomplished by flattening the array into 1d with ravel
+
+
+
+if rank == 1:
+    print("Received data:")
+    print(data)
